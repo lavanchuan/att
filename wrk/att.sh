68367557b97c2fdd3eebb52fb915c007c733614c
lavanchuan <chuan661999@gmail.com>
2025-08-08 00:56:10 +0700

    wrk

--- wrk/att.sh
+++ wrk/att.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 while true; do
-    wrk -t10 -c1000 -d5s -R20000 -s header.lua https://tanmong.online;
+    wrk -t10 -c1000 -d5s -s header.lua https://tanmong.online/;
     sleep 0;
 done
