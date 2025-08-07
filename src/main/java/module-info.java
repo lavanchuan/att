9acba31b12d8c139a7a61c5b7e8500514c0194c2
lavanchuan <chuan661999@gmail.com>
2025-08-07 23:09:47 +0700

    lag qua

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module demo.attackweb {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires playwright;
-
-
-    opens demo.attackweb to javafx.fxml;
-    exports demo.attackweb;
-}
